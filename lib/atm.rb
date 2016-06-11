eab666c4bc0609e819a2038afc38372b5da0057f
Lucia De Mojà <luciademoja@gmail.com>
2016-06-11 21:21:35 +0200

    Passing all the tests

--- lib/atm.rb
+++ lib/atm.rb
@@ -4,4 +4,23 @@ class Atm
   def initialize
     @funds = 1000
   end
+
+  def withdraw(amount, account)
+    case
+    when insufficient_funds_in_account?(amount, account) then
+      return
+    else
+      perform_transaction(amount, account)
+    end
+  end
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance 
+  end
+
+  def perform_transaction(amount, account)
+      @funds -= amount
+      account.balance = account.balance - amount
+      { status: true, message: 'success', date: Date.today, amount: amount }
+  end
 end
